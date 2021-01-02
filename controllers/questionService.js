c4fff8fdbaff864256759646a42d494a10de151f
Alejandro Guerrero <alexgue404@gmail.com>
2021-01-02 18:42:08 +0100

    Implementing add question endpoint

--- controllers/questionService.js
+++ controllers/questionService.js
@@ -1,5 +1,23 @@
 'use strict'
 
+
+var questionModel = require("../models/question.model");
+
+const responseOk = {
+  "code": 201,
+  "message": "Question created successfully"
+};
+
+const responseBodyIsMissing = {
+  "code": 400,
+  "message": "Request body is missing"
+};
+
+const responseUnexpectedError = {
+  "code": 500,
+  "message": "Unexpected error"
+};
+
 module.exports.getQuestions = function getQuestions(req, res, next) {
   res.send({
     message: 'This is the mockup controller for getQuestions'
@@ -7,15 +25,32 @@ module.exports.getQuestions = function getQuestions(req, res, next) {
 };
 
 module.exports.addQuestion = function addQuestion(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for addQuestion'
-  });
+  console.log(req.question.value);
+  if (!req.question.value) {
+    return res.status(400).send(responseBodyIsMissing);
+
+  }
+  var question = new questionModel(req.question.value);
+  console.log(question);
+
+  question.save()
+    .then(doc => {
+      if (!doc || doc.length === 0) {
+        return res.status(500).send(responseUnexpectedError);
+      }
+
+      res.status(201).send(responseOk);
+    })
+    .catch(err => {
+      res.status(500).json(responseUnexpectedError);
+    })
 };
 
 module.exports.findQuestionByid = function findQuestionByid(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for findQuestionByid'
-  });
+
+ questionModel.findOne({id: req.id.value}, function(err, question){
+  res.status(200).send(JSON.stringify(question))
+  })
 };
 
 module.exports.deleteQuestion = function deleteQuestion(req, res, next) {
