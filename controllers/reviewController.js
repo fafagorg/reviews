9e959029941e56554ee623348744bd0b06fd64c0
Juan Miguel Blanco <juanmiblancof@gmail.com>
2021-01-03 21:05:19 +0100

    @juablafer - #5 - Comment related endpoints
    * Implemented comment endpoints of reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -21,3 +21,27 @@ module.exports.deleteReview = function deleteReview(req, res, next) {
 module.exports.updateReview = function updateReview(req, res, next) {
   reviewService.updateReview(req.swagger.params, res, next);
 };
+
+module.exports.findReviewsByClientId = function findReviewsByClientId(req, res, next) {
+  reviewService.findReviewsByClientId(req.swagger.params, res, next);
+ };
+
+module.exports.deleteReviewsByClientId = function deleteReviewsByClientId(req, res, next) {
+ reviewService.deleteReviewsByClientId(req.swagger.params, res, next);
+};
+
+module.exports.findReviewsByProductId = function findReviewsByProductId(req, res, next) {
+ reviewService.findReviewsByProductId(req.swagger.params, res, next);
+};
+
+module.exports.deleteReviewsByProductId = function deleteReviewsByProductId(req, res, next) {
+ reviewService.deleteReviewsByProductId(req.swagger.params, res, next);
+};
+
+module.exports.findReviewCommentsById = function findReviewCommentsById(req, res, next) {
+  reviewService.findReviewCommentsById(req.swagger.params, res, next);
+};
+
+module.exports.addCommentToReview = function addCommentToReview(req, res, next) {
+  reviewService.addCommentToReview(req.swagger.params, res, next);
+};
