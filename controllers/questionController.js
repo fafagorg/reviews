ae5af22ca74c523d631fb60bdcadb3943973f3c1
Alejandro Guerrero <alexgue404@gmail.com>
2021-01-07 05:12:41 +0100

    feat: Implementing questions endpoints #14

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -1,23 +1,51 @@
 'use strict'
 
-var questionController = require('./questionService');
+var questionService = require('./questionService');
 
 module.exports.getQuestions = function getQuestions(req, res, next) {
- questionController.getQuestions(req.swagger.params, res, next);
+    questionService.getQuestions(req.swagger.params, res, next);
 };
 
 module.exports.addQuestion = function addQuestion(req, res, next) {
- questionController.addQuestion(req.swagger.params, res, next);
+    questionService.addQuestion(req.swagger.params, res, next);
 };
 
 module.exports.findQuestionByid = function findQuestionByid(req, res, next) {
- questionController.findQuestionByid(req.swagger.params, res, next);
+    questionService.findQuestionByid(req.swagger.params, res, next);
 };
 
 module.exports.deleteQuestion = function deleteQuestion(req, res, next) {
- questionController.deleteQuestion(req.swagger.params, res, next);
+    questionService.deleteQuestion(req.swagger.params, res, next);
 };
 
 module.exports.updateQuestion = function updateQuestion(req, res, next) {
- questionController.updateQuestion(req.swagger.params, res, next);
+    questionService.updateQuestion(req.swagger.params, res, next);
+};
+
+module.exports.findQuestionsByProductId = function findQuestionsByProductId(req, res, next) {
+    questionService.findQuestionsByProductId(req.swagger.params, res, next);
+};
+
+module.exports.deleteQuestionsByProductId = function deleteQuestionsByProductId(req, res, next) {
+    questionService.deleteQuestionsByProductId(req.swagger.params, res, next);
+};
+
+module.exports.findQuestionRepliesById = function findQuestionRepliesById(req, res, next) {
+    questionService.findQuestionRepliesById(req.swagger.params, res, next);
+};
+
+module.exports.addReplyToQuestion = function addReplyToQuestion(req, res, next) {
+    questionService.addReplyToQuestion(req.swagger.params, res, next);
+};
+
+module.exports.findQuestionSingleReply = function findQuestionSingleReply(req, res, next) {
+    questionService.findQuestionSingleReply(req.swagger.params, res, next);
+};
+
+module.exports.deleteReplyFromQuestion = function deleteReplyFromQuestion(req, res, next) {
+    questionService.deleteReplyFromQuestion(req.swagger.params, res, next);
+};
+
+module.exports.updateReplyFromQuestion = function updateReplyFromQuestion(req, res, next) {
+    questionService.updateReplyFromQuestion(req.swagger.params, res, next);
 };
